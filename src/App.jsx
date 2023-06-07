3495f9b6eaeb6f060fb1234dcac1ed9da694336f
Gaetano Izzo <gaetanoizzo2001@gmail.com>
2023-06-07 16:18:20 +0200

    Creato switch dimensionale

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import {  } from 'react'
-import { Container } from 'react-bootstrap';
 import './assets/css/colors.css'
+import Mobile from './size/mobile/Mobile';
+import Tablet from './size/tablet/Tablet';
+import Website from './size/website/Website';
 
 export default function App() {
   return (
     <>
-    <Container fluid>
-      
-    </Container>
+    {
+      window.innerWidth <  768 
+        ? <Mobile /> 
+        : window.innerWidth >=  768 && window.innerWidth <  1200 
+          ? <Tablet /> 
+          : <Website />
+    }      
     </>
   );
 }
