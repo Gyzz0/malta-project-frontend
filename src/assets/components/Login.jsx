f9b7a94b2b14ffaff5c9ccc5e3728d8f71aad4e9
Gaetano Izzo <gaetanoizzo2001@gmail.com>
2023-06-08 16:07:19 +0200

    Creato SwitchUtente

---
+++ src/assets/components/Login.jsx
@@ -0,0 +1,9 @@
+import { Container} from "react-bootstrap";
+
+export default function Login() {
+  return (
+    <Container fluid>
+      Login
+    </Container>
+  );
+}
---
+++ src/assets/components/Signup.jsx
@@ -0,0 +1,9 @@
+import { Container} from "react-bootstrap";
+
+export default function Signup() {
+  return (
+    <Container fluid>
+      Signup
+    </Container>
+  );
+}
