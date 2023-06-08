291957d2745c1e1ac7f8ace8f3a8d3b90039057e
Gaetano Izzo <gaetanoizzo2001@gmail.com>
2023-06-08 18:40:11 +0200

    Creato Form di Login

--- src/assets/components/Login.jsx
+++ src/assets/components/Login.jsx
@@ -1,9 +1,36 @@
-import { Container} from "react-bootstrap";
+import { Button, Form} from "react-bootstrap";
+import { clearFormInput, clearFormPassword } from "../js/form";
+import { notAuthPost } from "../js/not_auth";
 
-export default function Login() {
+export default function Login(props) {
+
+  async function loginSubmit(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const login = {username:clearFormInput(loginForm, 0), password:clearFormPassword(loginForm, 1)};
+    const response = JSON.parse(await notAuthPost("login", login));
+    console.log(login);
+    if(response){
+      localStorage.setItem("token",response);
+      location.reload();
+    }
+    else
+      alert("Username o password errate!")
+  }
+
   return (
-    <Container fluid>
-      Login
-    </Container>
+        <Form name="loginForm" onSubmit={loginSubmit}>
+          <Form.Group>
+            <Form.Label>Username</Form.Label>
+            <Form.Control placeholder="Username" type="text" name="username"></Form.Control>
+          </Form.Group>
+          <Form.Group className="mt-2">
+            <Form.Label>Password</Form.Label>
+            <Form.Control placeholder="Password" type="password" name="password"></Form.Control>
+          </Form.Group>
+          <Form.Group className="mt-3">
+            <Button type="submit" className="bg-nero border-rossoSangue hover-bg-borgogna w-100">Entra</Button>
+          </Form.Group>
+        </Form>
   );
 }
